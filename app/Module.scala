f15342192266eb358a6c6c1d0d581212ca29262c
Luca Milanesio <luca.milanesio@gmail.com>
2017-08-31 09:48:13 -0700

    Yah, work now
    Change-Id: I2ffbde20fe6755fc3534edc1b42a680d8b80867f

--- app/Module.scala
+++ app/Module.scala
@@ -23,6 +23,7 @@ class Module extends AbstractModule {
     bind(classOf[ApplicationTimer]).asEagerSingleton()
     // Set AtomicCounter as the implementation for Counter.
     bind(classOf[Counter]).to(classOf[AtomicCounter])
-//  }
+  }
+/// 
 
 }
